93ef224926f093bcc058352e1a4c4c15c2250438
Babiy <olegbabiy77@gmail.com>
2020-03-10 18:30:15 +0200

    Init project.

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import dog from "./assests/images/dog.png";
+import "./App.module.scss";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
