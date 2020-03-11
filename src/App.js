85a740ed23af23dc46b0cc00dd31e77187e2244b
Babiy <olegbabiy77@gmail.com>
2020-03-11 14:29:10 +0200

    Made Auth container.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { Route, Switch } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
 import Home from "./components/Sections/Home/Home";
+import Auth from "./containers/Auth/Auth";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Auth} />
         </Switch>
       </Layout>
     </div>
