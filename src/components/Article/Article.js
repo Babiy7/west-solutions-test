03f43e6512ed187a363594bfff985342fb443206
Babiy <olegbabiy77@gmail.com>
2020-03-13 18:30:53 +0200

    Fixed date.

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -2,21 +2,17 @@ import React from "react";
 import classes from "./Article.module.scss";
 
 const Article = props => {
+  const date = props.date;
+  const day = date.slice(0, 10);
+  const time = date.slice(12, date.length - 1);
+
   return (
     <div className={classes.Article}>
       <div className={classes.Header}>
-        <div className={classes.ImageContainer}>
-          <img className={classes.Image} src={props.image} alt="news" />
-        </div>
-        {/* <div
-          style={{
-            background: `url("${props.image}") center no-repeat`,
-            backgroundSize: "100% 100%",
-            height: "200px"
-          }}
-        ></div> */}
-        <h4 className={classes.Title}>{props.title}</h4>
+        <img className={classes.Image} src={props.image} alt="news" />
       </div>
+
+      <h4 className={classes.Title}>{props.title}</h4>
 
       <div className={classes.Content}>
         <p>{props.content}</p>
@@ -25,7 +21,9 @@ const Article = props => {
       <div className={classes.Footer}>
         <h2 className={classes.Author}>{props.author}</h2>
 
-        <div className={classes.Data}>{props.date}</div>
+        <div className={classes.Data}>
+          {day} <br /> {time}
+        </div>
       </div>
     </div>
   );
