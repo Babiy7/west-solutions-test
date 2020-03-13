63b7969c35b414e8e43d89e1e60967796c8e5eda
Babiy <olegbabiy77@gmail.com>
2020-03-13 18:18:20 +0200

    Fixed image in Article.

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,7 +5,16 @@ const Article = props => {
   return (
     <div className={classes.Article}>
       <div className={classes.Header}>
-        <img className={classes.Image} src={props.image} alt="news" />
+        <div className={classes.ImageContainer}>
+          <img className={classes.Image} src={props.image} alt="news" />
+        </div>
+        {/* <div
+          style={{
+            background: `url("${props.image}") center no-repeat`,
+            backgroundSize: "100% 100%",
+            height: "200px"
+          }}
+        ></div> */}
         <h4 className={classes.Title}>{props.title}</h4>
       </div>
 
