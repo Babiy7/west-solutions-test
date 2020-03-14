2abed8aeed5bde446e26bbdc871dde7a2fd8b465
Babiy <olegbabiy77@gmail.com>
2020-03-14 17:37:27 +0200

    Changed url request.

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-14&sortBy=publishedAt&apiKey=a2141125460b419eb95edb2eedcd474d"
+    "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a2141125460b419eb95edb2eedcd474d"
 });
 
 export default instance;
